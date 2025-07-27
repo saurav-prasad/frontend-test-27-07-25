968ecc00ca9315bf22a7f8d99c98f33f0e393d75
Saurav <saurav.prasad@hirehunch.com>
2025-07-27 16:48:47 +0530

    ui fixes

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -4,7 +4,7 @@ import styles from "./toolbar.module.css";
 export const Toolbar: React.FC = () => {
   return (
     <div className={styles.toolbar}>
-      <span>Scroll to see the magic 🪄</span>
+      <span>Scroll to see the magic happen🪄</span>
       <div className={styles.arrow}></div>
     </div>
   );
